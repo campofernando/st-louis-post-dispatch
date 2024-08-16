df9e03ca0819b8206b994d23826f4087067b29f8
Fernando Campo <campofernando594@gmail.com>
2024-08-16 06:00:06 -0300

    Create Xcode project

---
+++ st-louis-post-dispatch/st-louis-post-dispatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  st-louis-post-dispatch
+//
+//  Created by Fernando Campo Garcia on 16/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ st-louis-post-dispatch/st-louis-post-dispatchUITests/st_louis_post_dispatchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  st_louis_post_dispatchUITestsLaunchTests.swift
+//  st-louis-post-dispatchUITests
+//
+//  Created by Fernando Campo Garcia on 16/08/24.
+//
+
+import XCTest
+
+final class st_louis_post_dispatchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
